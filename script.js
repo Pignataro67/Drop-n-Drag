eb147db1dcfdf013c5971872c7d59aa5ad7a14e8
Pignataro67 <Pignataro67@gmail.com>
2022-08-28 11:47:41 -0700

    JS

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const empties = document.querySelectorAll('.empty')
 
 fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
+
+function dragStart() {
+    this.className += ' hold' 
+    setTimeout(() => this.className = 'invisible', 0)
+}
