d991586d24098775f44de9ac31782956c9a2e804
Pignataro67 <Pignataro67@gmail.com>
2022-08-29 05:25:30 -0700

    JS

--- script.js
+++ script.js
@@ -12,18 +12,23 @@ for(const empty of empties) {
 }
 
 function dragStart() {
-    this.className += ' hold' 
-    setTimeout(() => this.className = 'invisible', 0)
+  this.className += ' hold' 
+  setTimeout(() => this.className = 'invisible', 0)
 }
 
 function dragEnd() {
-    this.className = 'fill'
+  this.className = 'fill'
 }
 
 function dragOver(e) {
-    e.preventDefault()
+  e.preventDefault()
 }
 
 function dragOver(e) {
-    e.preventDefault()
+  e.preventDefault()
+}
+
+function dragEnter(e) {
+  e.preventDefault()
+  this.className += ' hovered'
 }
